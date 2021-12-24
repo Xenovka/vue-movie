002523e8250cde20dd46609dcc7b11368827af31
Xenovka <wendyanto18@gmail.com>
2021-12-24 13:24:27 +0700

    change navbar brand

--- src/components/BaseNavbar.vue
+++ src/components/BaseNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Vumovi</a>
+      <a class="navbar-brand" href="#">Vumovin</a>
       <button
         class="navbar-toggler"
         type="button"
