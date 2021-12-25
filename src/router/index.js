2dafdf280804af054d9e05916a1915dbe34101e8
Xenovka <wendyanto18@gmail.com>
2021-12-25 13:57:14 +0700

    movies route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import Home from "../views/Home.vue";
+import Movies from "../views/Movies.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home
+  },
+  {
+    path: "/movies",
+    name: "Movies",
+    component: Movies
   }
 ];
 
